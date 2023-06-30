df8b0dead2a59f3dc7004a82fff8b3b2783ebc67
mjsirfan2015 <mohammedirfan2012india@gmail.com>
2023-06-30 19:33:19 +0530

    google autocomplete component

---
+++ src/components/GoogleMap/GoogleAutoComplete.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import ReactGoogleAutocomplete from 'react-google-autocomplete';
+import { setPage } from 'src/store/reducers/registerReport';
+import { useDispatch } from 'react-redux';
+
+const GoogleAutoComplete = ({style}) => {
+  const dispatch = useDispatch();
+
+  const handlePlaceSelected = (place) => {
+    const {lat,lng}=place.geometry.location;
+    const {formatted_address:location,place_id:google_place_id} = place;
+    const latitude=lat();
+    const longitude=lng();
+    dispatch(setPage({latitude,longitude,location,google_place_id}));
+  };
+  return(
+    <ReactGoogleAutocomplete
+      apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}
+      options={{
+        bounds: new window.google.maps.LatLngBounds(
+          new window.google.maps.LatLng(-26.2751, 27.9639),
+          new window.google.maps.LatLng(-26.0313, 28.2336)
+        ),
+        types: ['geocode'],
+      }}
+      style={{height:"50px",padding:"10px",borderRadius:10,width:250,...style}}
+      onPlaceSelected={handlePlaceSelected}
+/>
+  )
+};
+
+export default GoogleAutoComplete;
