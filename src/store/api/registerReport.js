c44f56b2c1b8ab8ee956188446fc498feb67c882
mjsirfan2015 <mohammedirfan2012india@gmail.com>
2023-06-30 19:33:56 +0530

    registerreport state

---
+++ src/store/api/registerReport.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import API from 'src/config/api'
+
+export const getNearbyCrimes = createAsyncThunk("registerReport/getNearbyCrimes",async({lat,long})=>{
+    try {
+        const res = await API.post("/report/getNearByReport",{lat,long});
+        return res.data.data;
+    } catch (error) {
+        console.error(error);
+        throw Error(error.message);
+    }
+})
+
+export const googleMapSearchLocation = createAsyncThunk(
+    "reportRegister/location",
+    async ({}) => {
+      try {
+        const response = await API.get("https://maps.googleapis.com/maps/api/place/y/json?input=Paris&types=geocode&key=AIzaSyApoj80RTzWkAIc_eswUmPogeoufErlNaw", { headers : {"Access-Control-Allow-Origin" : "*"}});
+        response.headers({"Access-Control-Allow-Origin" : "*"});
+        if(response.data.status === 200){
+          return response.data.data;
+        }
+      } catch (err) {
+        console.error(err);
+        throw Error(err.message);
+      }
+    }
+  );
