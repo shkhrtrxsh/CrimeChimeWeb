30ecc43d5e5e5965c1142a15263da4a675bb0983
Shikhar Mishra <shkhrtrxsh@gmail.com>
2023-07-04 18:32:24 +0530

    bugfixes 8.0

--- src/layouts/Report/ProgressBar.jsx
+++ src/layouts/Report/ProgressBar.jsx
@@ -26,20 +26,26 @@ const ProgressBar = ({activeStep,beforeNext=null,beforeBack=null,nextLink=null,b
       width: isMdBreakpoint ? '42.9%' : '100%',
     }}
   >
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#ffe600', color: 'black', padding: '12px',width:"100%" }}>
-
-            {backLink?<NextButton beforeNext={lock?null:()=>setActiveStep(activeStep,activeStep-1)} textValue="GO BACK"/>:<Typography sx={{cursor:"pointer"}} variant="h6">CANCEL</Typography>}
+        <Box sx={{ display: 'flex', alignItems: 'center', backgroundColor: '#ffe600', color: 'black', padding: '12px',width:"100%" }}>
+<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: useMediaQuery(theme.breakpoints.up('md')) ? '33.3%' : '40%' }}>
+   {backLink?<NextButton beforeNext={lock?null:()=>setActiveStep(activeStep,activeStep-1)} textValue="GO BACK"/>:<Typography sx={{cursor:"pointer"}} variant="h6">CANCEL</Typography>}
+</Box>
+            <Divider orientation="vertical" flexItem style={{ backgroundColor: 'black', marginLeft: '8px', marginRight: '8px' }} />
+<Box  sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: useMediaQuery(theme.breakpoints.up('md')) ? '33.3%' : '10%' }}>
+  <Typography variant="h6" sx={{cursor:"default"}}>#{activeStep}/17</Typography>
+</Box>
+            
 
             <Divider orientation="vertical" flexItem style={{ backgroundColor: 'black', marginLeft: '8px', marginRight: '8px' }} />
-
-            <Typography variant="h6" sx={{cursor:"default"}}>#{activeStep}/17</Typography>
-
-            <Divider orientation="vertical" flexItem style={{ backgroundColor: 'black', marginLeft: '8px', marginRight: '8px' }} />
-
-            <NextButton textValue={submit?"SUBMIT":"NEXT"} beforeNext={lock?null:()=>{beforeNext?beforeNext():setActiveStep(activeStep,activeStep+1)}} />
+<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: useMediaQuery(theme.breakpoints.up('md')) ? '33.3%' : '10%' }}>
+   <NextButton textValue={submit?"SUBMIT":"NEXT"} beforeNext={lock?null:()=>{beforeNext?beforeNext():setActiveStep(activeStep,activeStep+1)}} />
+</Box>
+{isMdBreakpoint? null : <Divider orientation="vertical" flexItem style={{ backgroundColor: 'black', marginLeft: '8px', marginRight: '8px' }} />}
             {!isMdBreakpoint&&
-              <Box sx={{ display: 'flex', mx: 3, justifyContent: 'center', alignItems: 'center' }} onClick={() => { setCancel(false); }}>
+              <Box sx={{ display: 'flex', mx: 3, justifyContent: 'center', alignItems: 'center', width: "40%" }} onClick={() => { setCancel(false); }}>
+              
               <LocationOnIcon />
+              
               <Typography sx={{ cursor: 'pointer', textTransform: 'uppercase', textDecoration: 'underline' }} variant="h6">
                 VIEW MAP
               </Typography>
