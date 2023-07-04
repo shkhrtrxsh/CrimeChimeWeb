d97f0b73693e2f9670d0ea93d087b5289cc27ad8
Shikhar Mishra <shkhrtrxsh@gmail.com>
2023-07-04 15:54:00 +0530

    bugfix 5.0

--- src/pages/Frontend/AddReport/page7.jsx
+++ src/pages/Frontend/AddReport/page7.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Container,useTheme, 
+  Container,
+  useTheme,
   Typography,
   Grid,
-  TextField,
   Box,
   Select,
   MenuItem,
@@ -17,20 +17,20 @@ import { setLock, setPage } from 'src/store/reducers/registerReport';
 import ProgressBar from 'src/layouts/Report/ProgressBar';
 
 function Page7() {
-  const {assault:value,assault_people:count} = useSelector(state=>state.reportRegister.data);
+  const { assault: value, assault_people: count } = useSelector((state) => state.reportRegister.data);
   const dispatch = useDispatch();
-  const [error,setError] = useState("");
+  const [error, setError] = useState('');
 
-  const setValue=(assault)=>dispatch(setPage({assault}));
-  const setCount=(assault_people)=>dispatch(setPage({assault_people}));
+  const setValue = (assault) => dispatch(setPage({ assault }));
+  const setCount = (assault_people) => dispatch(setPage({ assault_people }));
 
   const theme = useTheme();
   const handleChange = (event) => {
-    const value = event.target.value
-    if(value==="0"||value==="2"){
-      dispatch(setLock(false))
+    const value = event.target.value;
+    if (value === '0' || value === '2') {
+      dispatch(setLock(false));
       setCount(null);
-    }else{
+    } else {
       setCount(1);
     }
     setValue(value);
@@ -38,75 +38,74 @@ function Page7() {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <Container>
-            <Grid container spacing={2} justifyContent="center" sx={{ paddingY: 0 }}>
-              <Grid item xs={10} sx={{ pt: 5,mt:5 }}>
-              <Box display="flex" alignItems="center" justifyContent="center">
-              <Box borderBottom={2} borderColor={theme.palette.warning.main} style={{ marginRight: '5px', width: '20px' }} />
-              <Typography variant="h1" align="center" style={{ fontWeight: 'bold', paddingBottom: '5px', fontSize: '24px' }}>
-                  Assault
+      <Container>
+        <Grid container spacing={2} justifyContent="center" sx={{ paddingY: 0 }}>
+          <Grid item xs={10} sx={{ pt: 5, mt: 5 }}>
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <Box
+                borderBottom={2}
+                borderColor={theme.palette.warning.main}
+                style={{ marginRight: '5px', width: '20px' }}
+              />
+              <Typography
+                variant="h1"
+                align="center"
+                style={{ fontWeight: 'bold', paddingBottom: '5px', fontSize: '24px' }}
+              >
+                Assault
+              </Typography>
+              <Box borderBottom={2} borderColor={theme.palette.warning.main} style={{ marginLeft: '5px', width: '20px' }} />
+            </Box>
+            <Typography variant="h2" align="center" style={{ fontWeight: 'bold', paddingBottom: '20px', fontSize: '12px' }}>
+              (Injuries due to violence)
+            </Typography>
+          </Grid>
+
+          <Box sx={{ py: 1 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', my: 4, pl: 4 }}>
+              <FormControlLabel control={<Checkbox checked={value === '0'} value={0} onChange={handleChange} />} label="Unknown" />
+              <FormControlLabel control={<Checkbox checked={value === '1'} value={1} onChange={handleChange} />} label="Yes" />
+              <FormControlLabel control={<Checkbox checked={value === '2'} value={2} onChange={handleChange} />} label="No" />
+              <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'start', alignItems: 'center', mt: 3 }}>
+                <Select
+                  value={count}
+                  onChange={(e) => {
+                    const count = e.target.value;
+                    if (!count || count <= 0) {
+                      dispatch(setLock(true));
+                      setError('*required');
+                    } else {
+                      dispatch(setLock(false));
+                      setError('');
+                    }
+                    setCount(count);
+                  }}
+                  disabled={value === '0' || value === '2'}
+                  sx={{ width: 100, marginRight: 2 }}
+                >
+                  {Array.from({ length: 30 }, (_, index) => index + 1).map((num) => (
+                    <MenuItem key={num} value={num}>
+                      {num}
+                    </MenuItem>
+                  ))}
+                </Select>
+                <Typography variant="h6" sx={{ fontWeight: 'normal', px: 2, textAlign: 'center' }}>
+                  If so, how many were assaulted?
                 </Typography>
-                <Box borderBottom={2} borderColor={theme.palette.warning.main} style={{ marginLeft: '5px', width: '20px' }} />
-                </Box>
-                <Typography variant="h2" align="center" style={{ fontWeight: 'bold', paddingBottom: '20px', fontSize: '12px' }}>
-                  (Injuries due to violence)
-                </Typography>
-              </Grid>
-
-
-              <Box sx={{ py: 1 }}>
-                <Box sx={{ display: 'flex', flexDirection: 'COLUMN', justifyContent: 'center', my: 4, pl: 4 }}>
-                <FormControlLabel
-                    control={<Checkbox checked={value==="0"} value={0} onChange={handleChange} />}
-                    label="Unknown"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox checked={value==="1"} value={1} onChange={handleChange} />}
-                    label="Yes"
-                  />
-                  <FormControlLabel
-                    control={<Checkbox checked={value==="2"} value={2} onChange={handleChange} />}
-                    label="No"
-                  />
-                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'start', alignItems: 'center', mt:3 }}>
-                  <TextField
-                    type="number"
-                    sx={{ px: 1,width:100 }}
-                    value={count}
-                    onChange={(e)=>{
-                      const count=e.target.value;
-                      if(!count||count<=0){
-                        dispatch(setLock(true))
-                        setError("*required")
-                      }else{
-                        dispatch(setLock(false))
-                        setError("")
-                      }
-                      setCount(count);
-                    }}
-                    disabled={value==="0"||value==="2"}
-                    InputProps={{
-                      inputProps: {
-                        min: 1,
-                        max: 10,
-                        step: 1,
-                      },
-                    }}
-                    error={error?true:false}
-                    helperText={error}
-                  />
-                  <Typography variant="h6" sx={{ fontWeight: 'normal', px: 2, textAlign: 'center' }}>
-                    If so, How many were Assaulted
-                  </Typography>
-                </Box>
-
-                </Box>
               </Box>
-            </Grid>
-          </Container>
+              {error && (
+                <Typography variant="body2" color="error" sx={{ mt: 1 }}>
+                  {error}
+                </Typography>
+              )}
+            </Box>
+          </Box>
+        </Grid>
+      </Container>
     </LocalizationProvider>
   );
 }
 
 export default Page7;
+
 
